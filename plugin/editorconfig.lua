5b9e303e1d6f7abfe616ce4cc8d3fffc554790bf
Gregory Anders <greg@gpanders.com>
2023-01-10 08:34:57 -0700

    Update variable name to disable builtin EditorConfig support

--- plugin/editorconfig.lua
+++ plugin/editorconfig.lua
@@ -1,13 +1,2 @@
 -- Disable builtin EditorConfig when using this plugin
-vim.g.editorconfig_enable = false
-
--- Once Neovim 0.9 is released, uncomment this
---[[
-if vim.fn.has('nvim-0.9') then
-    vim.notify_once(
-        "editorconfig.nvim: This plugin has been upstreamed to Neovim and is no longer necessary. Consider uninstalling.",
-        vim.log.levels.INFO,
-        { title = "editorconfig.nvim" }
-    )
-end
-]]
+vim.g.editorconfig = false
